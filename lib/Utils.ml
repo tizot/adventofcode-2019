235670aa5f45e75c01ad3b18aeb71722ec143f42
Camille Masset <masset.camille@gmail.com>
2019-12-07 20:54:43 +0100

    Add utils functions

---
+++ lib/Utils.ml
@@ -0,0 +1,12 @@
+open Printf
+
+let read_lines filename =
+  let lines = ref [] in
+  let chan = open_in filename in
+  try
+    while true; do
+      lines := input_line chan :: !lines
+    done; !lines
+  with End_of_file ->
+    close_in chan;
+    List.rev !lines
